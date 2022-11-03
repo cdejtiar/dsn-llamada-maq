c0fdfec4bf0a54c97c9258bf219b0e8fb7dbc101
MaxiCo1 <coletti.maxi@gmail.com>
2022-11-02 22:21:42 -0300

    arreglos

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const targetArrows = document.getElementById("arrows")
 
 const targetHangup = document.getElementById("end")
 
-/*const message = document.getElementById("message")
+const message = document.getElementById("message")
 
-const pickupMenu = document.getElementById("pickupMenu")*/
+const pickupMenu = document.getElementById("pickupScreen")
 const mainScreen = document.getElementById("mainScreen")
 
 function mainScreenOpacity () {
-    mainScreen.style.opacity = "0"
+    mainScreen.style.display = "none"
+}
+function pickupScreendisplay (){
+    pickupMenu.style.display = "inline-block"
 }
 
 function atender(){
@@ -24,13 +27,12 @@ targetPickup.style.opacity = "0"
 targetArrows.style.transition = "opacity 1.5s ease-in-out"
 targetArrows.style.opacity = "0"
 setTimeout(mainScreenOpacity, 1000)
-
-/*targetHangup.style.opacity = "0"
+targetHangup.style.opacity = "0"
 targetHangup.style.transition = "opacity 1.5s ease-in-out"
 message.style.transition = "opacity 1.5s ease-in-out"
 message.style.opacity = "0"
-pickupMenu.style.display = "inline-block"*/
-//targetPickup.classList.add("slide-up")
+setTimeout(pickupScreendisplay, 1000)
+targetPickup.classList.add("slide-up")
 }
 
 function colgar(){
@@ -38,12 +40,7 @@ function colgar(){
     targetHangup.style.transition = "1s ease-in-out"
     targetHangup.style.opacity = "0"
 }
-const count = 0;
-const segundos = document.getElementById("seconds")
-function timeCount(){
-    count + 1;
-    segundos.innerHTML = count
-}
+
 
 function callTime(){
     setTimeout(timeCount, 1000)
@@ -51,7 +48,11 @@ function callTime(){
 
 
 function clase(){
-    setInterval(shake, 3000)  
+    setInterval(shake, 3000)
+
+        setInterval(cronometro, 1000)
+    
+    
 }
 
 function shake(){
@@ -62,3 +63,27 @@ function shake(){
     }
 }
 
+var segundos = 0;
+var minutos = 0;
+
+let secs = document.getElementById("seconds")
+let mins = document.getElementById("minutes")
+
+
+function cronometro(){
+    
+    if(segundos < 9){
+        segundos++;
+        seconds.innerHTML = "0" + segundos;
+    } else{
+       segundos++;
+        seconds.innerHTML = segundos; 
+    }
+    
+    if (segundos == 59) {
+		segundos = -1;
+        minutos++;
+        mins.innerHTML = mins
+	}
+    
+}
